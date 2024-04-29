7de7452ae5b2fcda9035851202e7165beb07d089
azzazhr <azzahraqina70@gmail.com>
2024-04-29 10:52:11 +0700

    add ClassNasabah(percobaann2)

---
+++ minggu10/Nasabah07.java
@@ -0,0 +1,105 @@
+package minggu10;
+
+public class Nasabah07 {
+
+    String norek, nama, alamat;
+    int umur;
+    double saldo;
+
+    Nasabah07(String norek, String nama, String alamat, int umur, double saldo) {
+        this.norek = norek;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.umur = umur;
+        this.saldo = saldo;
+    }
+
+    Nasabah07[] data;
+    int front, rear, size, max;
+
+    public Nasabah07(int n) {
+        max = n;
+        data = new Nasabah07[max];
+        size = 0;
+        front = rear = -1;
+    }
+
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isFull() {
+        if (size == max) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void Enqueue(Nasabah07 dt) {
+        if (isFull()) {
+            System.out.println("Queue sudah penuh");
+        } else {
+            if (isEmpty()) {
+                front = rear = 0;
+            } else {
+                if (rear == max - 1) {
+                    rear = 0;
+                } else {
+                    rear++;
+                }
+            }
+            data[rear] = dt;
+            size++;
+        }
+    }
+
+    public Nasabah07 Dequeue() {
+        Nasabah07 dt = null;
+        if (isEmpty()) {
+            System.out.println("Queue masih kosong");
+        } else {
+            dt = data[front];
+            size--;
+            if (isEmpty()) {
+                front = rear = -1;
+            } else {
+                if (front == max - 1) {
+                    front = 0;
+                } else {
+                    front++;
+                }
+            }
+        }
+        return dt;
+    }
+
+    public void peek() {
+        if (!isEmpty()) {
+            System.out.println("Elemen terdepan: " + data[front].norek + " " + data[front].nama + " "
+                    + data[front].alamat + " " + data[front].umur + " " + data[front].saldo);
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+
+    public void print() {
+        if (isEmpty()) {
+            System.out.println("Queue masih kosong");
+        } else {
+            int i = front;
+            while (i != rear) {
+                System.out.println(data[i].norek + " " + data[i].nama + " " + data[i].alamat + " " + data[i].umur + " "
+                        + data[i].saldo);
+                i = (i + 1) % max;
+            }
+            System.out.println(data[i].norek + " " + data[i].nama + " " + data[i].alamat + " " + data[i].umur + " "
+                    + data[i].saldo);
+            System.out.println("Jumlah elemen = " + size);
+        }
+    }
+}
