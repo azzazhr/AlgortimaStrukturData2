9df55927db96c1c6c56da3d15001d161162f09e2
azzazhr <azzahraqina70@gmail.com>
2024-05-03 09:55:03 +0700

    add ClassNode(percobaan1 pembuatan linked list)

---
+++ minggu11/Node07.java
@@ -0,0 +1,15 @@
+package minggu11;
+
+public class Node07 {
+    int data;
+    Node07 next;
+
+    Node07(int nilai, Node07 berikutnya) {
+        data = nilai;
+        next = berikutnya;
+    }
+
+    Node07() {
+
+    }
+}
