4ebbe45bdaa428aa38fc8955bf052834b602016a
azzazhr <azzahraqina70@gmail.com>
2024-05-07 08:27:02 +0700

    add ClassNode(Tugas No2)

---
+++ minggu11/Tugas2/NodeTugas207.java
@@ -0,0 +1,15 @@
+package minggu11.Tugas2;
+
+public class NodeTugas207 {
+    int nim;
+    String nama;
+    String keperluan;
+    NodeTugas207 next;
+
+    public NodeTugas207(int nim, String nama, String keperluan) {
+        this.nim = nim;
+        this.nama = nama;
+        this.keperluan = keperluan;
+        this.next = null;
+    }
+}
