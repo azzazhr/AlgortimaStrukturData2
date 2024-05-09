9fa8765d393cfb67193b86d32ccbeca8dfc01618
azzazhr <azzahraqina70@gmail.com>
2024-05-09 10:15:59 +0700

    add Main DoubleLinkedList(tambahan percobaan2)

--- minggu12/DoubleLinkedListMain07.java
+++ minggu12/DoubleLinkedListMain07.java
@@ -23,6 +23,25 @@ public class DoubleLinkedListMain07 {
         dll.celar();
         dll.print();
         System.out.println("Size : " + dll.size());
-
+        System.out.println("============================");
+        dll.addLast(50);
+        dll.addLast(40);
+        dll.addLast(10);
+        dll.addLast(20);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("============================");
+        dll.removeFirst();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("============================");
+        dll.removeLast();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("============================");
+        dll.remove(1);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("============================");
     }
 }
