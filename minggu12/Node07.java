b0d0765e386a13cc0fa7d9fb69c85147a7dabeed
azzazhr <azzahraqina70@gmail.com>
2024-05-09 09:30:03 +0700

    add ClassNode07(percobaan1)

---
+++ minggu12/Node07.java
@@ -0,0 +1,12 @@
+package minggu12;
+
+public class Node07 {
+    int data;
+    Node07 prev, next;
+
+    Node07(Node07 prev, int data, Node07 next) {
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+    }
+}
