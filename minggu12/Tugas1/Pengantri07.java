f401683a1a1bc17dc9ffdeb717fd31e4281cd788
azzazhr <azzahraqina70@gmail.com>
2024-05-19 19:25:24 +0700

    add ClassPengantri(Tugas01)

---
+++ minggu12/Tugas1/Pengantri07.java
@@ -0,0 +1,11 @@
+package minggu12.Tugas1;
+
+public class Pengantri07 {
+    int no;
+    String nama;
+
+    Pengantri07(int no, String nama) {
+        this.no = no;
+        this.nama = nama;
+    }
+}
