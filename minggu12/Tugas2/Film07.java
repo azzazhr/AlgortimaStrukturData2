4222a2f0f3844c3642f81ea33155d5e03b4a3369
azzazhr <azzahraqina70@gmail.com>
2024-05-19 19:26:43 +0700

    add ClassFilm(Tugas02)

---
+++ minggu12/Tugas2/Film07.java
@@ -0,0 +1,13 @@
+package minggu12.Tugas2;
+
+public class Film07 {
+    int id;
+    String judul;
+    float rating;
+
+    Film07(int id, String judul, float rating) {
+        this.id = id;
+        this.judul = judul;
+        this.rating = rating;
+    }
+}
